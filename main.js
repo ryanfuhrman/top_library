063e76b3d4cf0fa3afbce33f71b3302e1345ff15
Ryan Fuhrman <rsfuhrman10@gmail.com>
2021-09-08 10:32:31 -0400

    Refactored function constructors into a Class

--- main.js
+++ main.js
@@ -8,91 +8,184 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-let myLibrary = [
-  new Book("Extreme Ownership", "Some Seals", 324, true),
-  new Book("Leviathan Wakes", "James S.A. Corey", 865, true),
-  new Book("Slaughterhouse-Five", "Kurt Vonnegut Jr.", 245, true),
-  new Book("Recursion", "Blake Crouch", 579, false),
-];
+class Shelf {
+  myLibrary = [
+    new Book("Extreme Ownership", "Some Seals", 324, true),
+    new Book("Leviathan Wakes", "James S.A. Corey", 865, true),
+    new Book("Slaughterhouse-Five", "Kurt Vonnegut Jr.", 245, true),
+    new Book("Recursion", "Blake Crouch", 579, false),
+  ];
 
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pages}, ${
-    this.read === true ? `read` : `not read yet`
-  }.`;
-};
+  info = () => {
+    return `${this.title} by ${this.author}, ${this.pages}, ${
+      this.read === true ? `read` : `not read yet`
+    }.`;
+  };
 
-Book.prototype.updateReadStatus = function () {
-  return !this.read;
-};
+  addBookToLibrary = (book) => {
+    // do stuff here
+    return this.myLibrary.push(book);
+  };
 
-function addBookToLibrary(book) {
-  // do stuff here
-  return myLibrary.push(book);
-}
+  handleBookSubmisson = (e) => {
+    e.preventDefault();
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let readStatus = document.querySelector("#read").checked;
 
-function displayBooks(books) {
-  booksList.innerHTML = "";
-  books.map(function ({ title, author, pages, read }, i) {
-    let bookLi = document.createElement("li");
-    bookLi.className = "book-list-item";
-    bookLi.innerHTML = `
-      <div id="${i}">
-        <p class="book-title">${title}</h1>
-        <p class="book-author">${author}</p>
-        <p class="book-pages">${pages}</p>
-        <p class="book-read">${read}</p>
-        <button class="update-read-status-button">Update Status</button>
-        <button class="remove-book-button">Remove Book</button>
-      </div>
-    `;
+    let newBook = new Book(
+      `${title}`,
+      `${author}`,
+      `${pages}`,
+      `${readStatus}`
+    );
 
-    booksList.appendChild(bookLi);
-    addButtonLogic();
-  });
-}
+    this.addBookToLibrary(newBook);
+    this.displayBooks(this.myLibrary);
+  };
 
-function handleBookSubmisson(e) {
-  e.preventDefault();
-  let title = document.querySelector("#title").value;
-  let author = document.querySelector("#author").value;
-  let pages = document.querySelector("#pages").value;
-  let readStatus = document.querySelector("#read").checked;
+  addButtonLogic = () => {
+    let removeBookButton = document.querySelectorAll(".remove-book-button");
+    let updateReadStatusButton = document.querySelectorAll(
+      ".update-read-status-button"
+    );
 
-  let newBook = new Book(`${title}`, `${author}`, `${pages}`, `${readStatus}`);
+    for (let i = 0; i < removeBookButton.length; i++) {
+      removeBookButton[i].addEventListener("click", this.handleRemoveBook);
+    }
 
-  addBookToLibrary(newBook);
-  displayBooks(myLibrary);
-}
+    for (let i = 0; i < updateReadStatusButton.length; i++) {
+      updateReadStatusButton[i].addEventListener(
+        "click",
+        this.handleUpdateReadStatus
+      );
+    }
+  };
 
-function addButtonLogic() {
-  let removeBookButton = document.querySelectorAll(".remove-book-button");
-  let updateReadStatusButton = document.querySelectorAll(
-    ".update-read-status-button"
-  );
+  handleRemoveBook = (e) => {
+    let bookToRemove = e.target.parentNode.id;
+    myLibrary.splice(bookToRemove, 1);
 
-  for (let i = 0; i < removeBookButton.length; i++) {
-    removeBookButton[i].addEventListener("click", handleRemoveBook);
+    displayBooks(myLibrary);
+  };
+
+  handleUpdateReadStatus = (e) => {
+    let bookID = e.target.parentNode.id;
+    this.myLibrary[bookID].read = !this.myLibrary[bookID].read;
+
+    this.displayBooks(this.myLibrary);
+  };
+
+  displayBooks(books) {
+    booksList.innerHTML = "";
+    books.map(({ title, author, pages, read }, i) => {
+      let bookLi = document.createElement("li");
+      bookLi.className = "book-list-item";
+      bookLi.innerHTML = `
+        <div id="${i}">
+          <p class="book-title">${title}</h1>
+          <p class="book-author">${author}</p>
+          <p class="book-pages">${pages}</p>
+          <p class="book-read">${read}</p>
+          <button class="update-read-status-button">Update Status</button>
+          <button class="remove-book-button">Remove Book</button>
+        </div>
+      `;
+
+      booksList.appendChild(bookLi);
+      this.addButtonLogic();
+    });
   }
-
-  for (let i = 0; i < updateReadStatusButton.length; i++) {
-    updateReadStatusButton[i].addEventListener("click", handleUpdateReadStatus);
-  }
 }
 
-function handleRemoveBook(e) {
-  let bookToRemove = e.target.parentNode.id;
-  myLibrary.splice(bookToRemove, 1);
+// let myLibrary = [
+//   new Book("Extreme Ownership", "Some Seals", 324, true),
+//   new Book("Leviathan Wakes", "James S.A. Corey", 865, true),
+//   new Book("Slaughterhouse-Five", "Kurt Vonnegut Jr.", 245, true),
+//   new Book("Recursion", "Blake Crouch", 579, false),
+// ];
 
-  displayBooks(myLibrary);
-}
+// Book.prototype.info = function () {
+//   return `${this.title} by ${this.author}, ${this.pages}, ${
+//     this.read === true ? `read` : `not read yet`
+//   }.`;
+// };
 
-function handleUpdateReadStatus(e) {
-  let bookID = e.target.parentNode.id;
-  myLibrary[bookID].read = myLibrary[bookID].updateReadStatus();
+// Book.prototype.updateReadStatus = function () {
+//   return !this.read;
+// };
 
-  displayBooks(myLibrary);
-}
+// function addBookToLibrary(book) {
+//   // do stuff here
+//   return myLibrary.push(book);
+// }
 
-displayBooks(myLibrary);
+// function displayBooks(books) {
+//   booksList.innerHTML = "";
+//   books.map(function ({ title, author, pages, read }, i) {
+//     let bookLi = document.createElement("li");
+//     bookLi.className = "book-list-item";
+//     bookLi.innerHTML = `
+//       <div id="${i}">
+//         <p class="book-title">${title}</h1>
+//         <p class="book-author">${author}</p>
+//         <p class="book-pages">${pages}</p>
+//         <p class="book-read">${read}</p>
+//         <button class="update-read-status-button">Update Status</button>
+//         <button class="remove-book-button">Remove Book</button>
+//       </div>
+//     `;
 
-addBookSubmit.addEventListener("click", handleBookSubmisson);
+//     booksList.appendChild(bookLi);
+//     addButtonLogic();
+//   });
+// }
+
+// function handleBookSubmisson(e) {
+//   e.preventDefault();
+//   let title = document.querySelector("#title").value;
+//   let author = document.querySelector("#author").value;
+//   let pages = document.querySelector("#pages").value;
+//   let readStatus = document.querySelector("#read").checked;
+
+//   let newBook = new Book(`${title}`, `${author}`, `${pages}`, `${readStatus}`);
+
+//   addBookToLibrary(newBook);
+//   displayBooks(myLibrary);
+// }
+
+// function addButtonLogic() {
+//   let removeBookButton = document.querySelectorAll(".remove-book-button");
+//   let updateReadStatusButton = document.querySelectorAll(
+//     ".update-read-status-button"
+//   );
+
+//   for (let i = 0; i < removeBookButton.length; i++) {
+//     removeBookButton[i].addEventListener("click", handleRemoveBook);
+//   }
+
+//   for (let i = 0; i < updateReadStatusButton.length; i++) {
+//     updateReadStatusButton[i].addEventListener("click", handleUpdateReadStatus);
+//   }
+// }
+
+// function handleRemoveBook(e) {
+//   let bookToRemove = e.target.parentNode.id;
+//   myLibrary.splice(bookToRemove, 1);
+
+//   displayBooks(myLibrary);
+// }
+
+// function handleUpdateReadStatus(e) {
+//   let bookID = e.target.parentNode.id;
+//   myLibrary[bookID].read = myLibrary[bookID].updateReadStatus();
+
+//   displayBooks(myLibrary);
+// }
+
+let newShelf = new Shelf();
+
+newShelf.displayBooks(newShelf.myLibrary);
+
+addBookSubmit.addEventListener("click", newShelf.handleBookSubmisson);
